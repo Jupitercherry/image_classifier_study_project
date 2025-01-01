572e8be911448bde5ba4a67632ef43e3983f5f73
Amita <amikin85@gmail.com>
2025-01-01 21:24:52 +0000

    label creation

--- data/get_pet_labels.py
+++ data/get_pet_labels.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: amita
+# PROGRAMMER: Amita Kini Ravindra
 # DATE CREATED:   01.01.2025                               
 # REVISED DATE: 
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
@@ -42,4 +42,31 @@ def get_pet_labels(image_dir):
     """
     # Replace None with the results_dic dictionary that you created with this
     # function
-    return None
+    results_dic =dict()
+    pet_labels = list()
+    
+    # read from the the images dir and  store file names in a list 
+    filenames = listdir(image_dir)
+    # create pet labeles by cleaning the data
+    for idx1 in range ( 0,len(filenames),1):
+         pet_name=""
+         pet_labels_lower= filenames[idx1].lower()
+         pet_labels_split=pet_labels_lower.split("_")
+         for word in pet_labels_split:
+              if word.isalpha():
+                   pet_name += word + " "
+         pet_name =pet_name.strip()
+         pet_labels.append(pet_name)
+
+    # check the length of the list and write a loop to get the key
+    for idx in range( 0,len(filenames),1):
+        if filenames[idx] not in results_dic :
+            results_dic[filenames[idx]]=[pet_labels[idx]]
+        else :
+            print ( "warning key=",filenames[idx], "exists in results_dic with value =",results_dic[filenames[idx]])
+    print (results_dic)
+    return (results_dic)
+#def main ():
+   #get_pet_labels("/workspace/cd0184/9664b117-d773-4799-b6a3-d4640ed70218/data/pet_images")
+#main ()
+
